92b763a1724229662b96d658ae455aa012efb851
Daniel Hultmark <danielhultmark@gmail.com>
2025-09-19 10:32:10 +0200

    LAde in en metod

--- Program.cs
+++ Program.cs
@@ -17,67 +17,69 @@
                 int number = random.Next(1, 21);
                 //Här valde jag att skriva flera rader för att strukturera upp texten så att det inte bara blev en lång string
                 //Även random ligger här för att slumpa fram ett nytt nummer varje gång
-
-                for (int n = 5; n > 0; n--) //Här skapade jag en for-loop som räknar ner från 5 så att användaren bara får 5 försök
+        static void Spela()
                 {
-                    //Jag använde if satser för att se om användare gissade rätt eller fel
-                    //Jag använde också TryParse för att felhantera felaktiga inmatningar
-                    if (!int.TryParse(Console.ReadLine(), out int guess))
+                    for (int n = 5; n > 0; n--) //Här skapade jag en for-loop som räknar ner från 5 så att användaren bara får 5 försök
                     {
-                        Console.WriteLine("Din gissning är utanför 1-20, försök igen!");
-                        n++;//Lade in n++ så att användare inte blir av med ett försöka om hen matar in tal utanför parametern.
-                        continue;
+                        //Jag använde if satser för att se om användare gissade rätt eller fel
+                        //Jag använde också TryParse för att felhantera felaktiga inmatningar
+                        if (!int.TryParse(Console.ReadLine(), out int guess))
+                        {
+                            Console.WriteLine("Din gissning är utanför 1-20, försök igen!");
+                            n++;//Lade in n++ så att användare inte blir av med ett försöka om hen matar in tal utanför parametern.
+                            continue;
+
+                        }
+                        if (guess == number)
+                        {
+                            Console.WriteLine("Grattis! Du gissade rätt!");
+                            break; // ja in break här så att loppen inte fortsatte när användaren gissade rätt
+                        }
+                        else if (guess >= number)
+                        {
+                            Console.WriteLine("Talet är för högt!");
+
+                        }
+                        else if (guess <= number)
+                        {
+                            Console.WriteLine("Talet är för lågt!");
+
+                        }
+
+                        if (n > 1)
+                        {
+                            Console.WriteLine($"Du har {n - 1} försök kvar");
+                        }   //Lade in en extra if sats för att skriva ut hur många försök som är kvar
+
+                        else
+                        {
+
+                            Console.WriteLine("Du har inga försök kvar! Korrekt svar var: " + (number));
+                            break;//När användarens försök tar loopen slut och rätt nummer skrivs ut
+                        }
+
 
                     }
-                    if (guess == number)
+                    //här nere lade jag nästa if sats i while loopen så att användaren får frågan om hen vill försöka igen
+
+                    Console.WriteLine("Vill du spela igen? (j/n)");
+                    string svar = Console.ReadLine().ToLower();//skapar en string av svaret för att lägga in i if-satsen samt ToLower så att det små bokstäver även om användaren skriver stora bokstäver
+                    if (svar == "j")
                     {
-                        Console.WriteLine("Grattis! Du gissade rätt!");
-                        break; // ja in break här så att loppen inte fortsatte när användaren gissade rätt
-                    }
-                    else if (guess >= number)
-                    {
-                        Console.WriteLine("Talet är för högt!");
+                        spelaIgen = true;
 
                     }
-                    else if (guess <= number)
+                    else if (svar == "n")
                     {
-                        Console.WriteLine("Talet är för lågt!");
-
+                        spelaIgen = false;
+                        Console.WriteLine("Tack för att du spelade!");
+                        break;
                     }
-
-                    if (n > 1)
-                    {
-                        Console.WriteLine($"Du har {n - 1} försök kvar");
-                    }   //Lade in en extra if sats för att skriva ut hur många försök som är kvar
-
                     else
                     {
-
-                        Console.WriteLine("Du har inga försök kvar! Korrekt svar var: " + (number));
-                        break;//När användarens försök tar loopen slut och rätt nummer skrivs ut
+                        Console.WriteLine("Felaktigt svar, skriv j/n");
+                        //Lade även in en else ifall anvädare inte svarar med j eller n
                     }
-
-
-                }
-                //här nere lade jag nästa if sats i while loopen så att användaren får frågan om hen vill försöka igen
-                    
-                Console.WriteLine("Vill du spela igen? (j/n)");
-                string svar = Console.ReadLine().ToLower();//skapar en string av svaret för att lägga in i if-satsen samt ToLower så att det små bokstäver även om användaren skriver stora bokstäver
-                if (svar == "j")
-                {
-                    spelaIgen = true;
-                    
-                }
-                else if (svar == "n")
-                {
-                    spelaIgen = false;
-                    Console.WriteLine("Tack för att du spelade!");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Felaktigt svar, skriv j/n");
-                    //Lade även in en else ifall anvädare inte svarar med j eller n
                 }
 
             }
